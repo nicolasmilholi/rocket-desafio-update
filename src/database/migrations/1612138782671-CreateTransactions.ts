580cdbcd5af0be7646587d94d5fcd6dbd8c7c1d3
Nicolas <milholi51@gmail.com>
2021-02-01 21:24:07 -0300

    teste

---
+++ src/database/migrations/1612138782671-CreateTransactions.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTransactions1612138782671 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
